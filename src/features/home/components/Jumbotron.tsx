7f24616ed369c075243ec88bf3cdd651f59f1f88
Ahdan24 <Auvailable@gmail.com>
2024-12-17 09:33:10 +0700

    feat: search debounce dark mode

--- src/features/home/components/Jumbotron.tsx
+++ src/features/home/components/Jumbotron.tsx
@@ -1,9 +1,9 @@
 
 const Jumbotron = () => {
   return (
-    <section className="mt-20 flex flex-col items-center">
-      <h1 className="text-6xl font-bold">The <span className="bg-orange-400"> PornHub </span></h1>
-      <p className="text-xl">A blog about food, experiences, and style</p>
+    <section className="mt-10 md:mt-20 flex flex-col items-center">
+      <h1 className="text-4xl md:text-6xl font-bold">The <span className="bg-orange-500"> BlogHub </span></h1>
+      <p className="md:text-xl">A blog about Blog, experiences, and style</p>
     </section>
   );
 };
