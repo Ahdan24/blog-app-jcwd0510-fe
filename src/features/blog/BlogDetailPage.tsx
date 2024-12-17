89f6ffbfc02cc1ee3a10dbba8269bbc5aa4964bd
Muhammad Ahdan Aufa <156409996+Ahdan24@users.noreply.github.com>
2024-12-17 10:22:37 +0700

    feat: auto logout (#6)

--- src/features/blog/BlogDetailPage.tsx
+++ src/features/blog/BlogDetailPage.tsx
@@ -45,7 +45,10 @@ const BlogDetailPage: FC<BlogDetailPageProps> = ({ blogId }) => {
           <p>
             {format(new Date(data.createdAt), "dd MMM yyyy")} - {data.user.name}
           </p>
-          {id === data.userId && <ModalDelete onClick={onClickDeleteBlog} />}
+          {id === data.userId && 
+          <ModalDelete
+          onClick={onClickDeleteBlog}
+          isPending={isPendingDelete} />}
         </div>
 
         <div className="relative h-[200px] bg-gray-500 md:h-[400px]">
